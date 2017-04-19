b0499be2ca40f46c51861981df6e45fe703f1481
Niels uit de Bos <nielius@gmail.com>
2017-04-19 16:50:46 +0200

    Kleine comment toegevoegd

--- bin/main.py
+++ bin/main.py
@@ -92,7 +92,7 @@ be running."""
     # should work, except that the function names are of course wrong; might
     # also need to trim the quotes from the returned string; or use princ in
     # the emacs command?
-    emacsoutput = subprocess.check_output("emacsclient-snapshot -e '(base64-encode-string (org-export-string-as (base64-decode-string \"{}\") '\"'\"'latex t) t)'".format(b64encodestring(s)), shell=True)
+    emacsoutput = subprocess.check_output("emacsclient-snapshot -e '(base64-encode-string (org-export-string-as (base64-decode-string \"{}\") '\"'\"'latex t) t)'".format(b64encodestring(s)), shell=True) # de gekke '\"'\"' is om bash een ' door te laten geven aan emacs
     # Note: the t in emacs's base64-encode-string ensures that there are no newlines in the output.
 
     # now we need to format the output (which is a bytetype object of a b64 string plus newlines and quotes) and decode it
